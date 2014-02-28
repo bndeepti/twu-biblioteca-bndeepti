a16d0ef1adeb5a3a233d07308852b419f4069e09
bndeepti@gmail.com <bndeepti@gmail.com@3dcd0d16-ad9e-5f8a-6d66-c36c425ba8b9>
2014-02-28 04:43:39 +0000

    second commit

--- trunk/src/com/twu/biblioteca/Library.java
+++ trunk/src/com/twu/biblioteca/Library.java
@@ -1,115 +1,53 @@
+
 package com.twu.biblioteca;
-
 import java.util.ArrayList;
-import java.util.List;
-import java.lang.*;
-
+import java.util.Iterator;
 
 /**
- * Created by deeptibn on 2/24/14.
+ * Created by deeptibn on 2/27/14.
  */
 public class Library {
 
-    public List<String> list_of_books=new ArrayList<String>();
-    public static String status_msg="";
-    public Library(){
-        list_of_books.add("Da Vinci Code");
-        list_of_books.add("The Alchemist");
-        list_of_books.add("Harry Potter");
+    ArrayList<String> list_of_books;
+    ArrayList<String> list_of_menu;
+    Customer customer;
+
+    public Library(Customer customer){
+        list_of_books=new ArrayList<String>();
+        list_of_books.add("Alchemist");
+        list_of_books.add("Inferno");
+        list_of_menu=new ArrayList<String>();
+        list_of_menu.add("List of books");
+        list_of_menu.add("Check Out");
+        list_of_menu.add("Check In");
+        list_of_menu.add("Exit");
+        this.customer=customer;
     }
-    public void AddBooks(){
+
+    public String DisplayListOfBooks() {
+        return list_of_books.toString();
+    }
 
 
-    }
-    public List<String> FetchList() {
-        //list_of_books.add("Da Vinci Code");
-       // list_of_books.add("The Alchemist");
-        //list_of_books.add("Harry Potter");
-        //System.out.println(list_of_books);
-        /*List<String> s=new ArrayList<String>();
-        for(Book b: list_of_books){
-            s.add(b.Name);
-            System.out.println(b.Name+" "+b.copies);
-        }*/
-        return list_of_books;
-    }
-    public List<String> CheckOut(String book){
-
-        if(list_of_books.contains(book)){
-        Customer.books[Customer.no_of_books]=book;
-        Customer.no_of_books++;
+    public String CheckOut(String book) {
+        if(list_of_books.contains(book)&&!customer.books_held.contains(book)){
+            customer.books_held.add(book);
             list_of_books.remove(book);
-            status_msg="Thank you! Enjoy the book";
+            return "Thank you! Enjoy the book";
         }
-
-        /*int flag=0;
-        int n=-1;
-            for(Book b: list_of_books){
-                if(b.Name.equals(book)){
-                    //Book bookChosen=b;
-                    b.copies=b.copies-1;
-                    flag=1;
-                    FetchList();
-                    n=b.copies;
-
-                }
-            }
-        if(flag==0)
-        {
-            System.out.println("Book not valid.Please view the list of books");
-            n=-1;
-        }
-        return n;*/
-        //if(list_of_books.contains(book)){
-          //  list_of_books.remove(book);
-        //System.out.print("Thank you! Enjoy the book");
-        //}
-        else{
-            //System.out.print("That book is not available.");
-            status_msg="That book is not available";
-        }
-        for(int index=0;index<Customer.no_of_books;index++)
-            System.out.println(Customer.books[index]);
-        return list_of_books;
-
-
+        else
+            return "That book is not available";
     }
-    public List<String> CheckIn(String book){
-        int flag=0;
-        /*int flag=0;
-        int n=-1;
-            for(Book b: list_of_books){
-                if(b.Name.equals(book)){
-                    //Book bookChosen=b;
-                    b.copies=b.copies+1;
-                    FetchList();
-                    n=b.copies;
-                    flag=1;
-                }
-            }
-        if(flag==0)
-        {
-            System.out.println("Book not valid.Please view the list of books");
-            n=-1;
+
+    public String CheckIn(String book) {
+        if(!list_of_books.contains(book)&&customer.books_held.contains(book)){
+            customer.books_held.remove(book);
+            list_of_books.add(book);
+            return "Thank you for returning the book";
         }
-        FetchList();
-        return n;*/
-        if(!list_of_books.contains(book)){
-            for (int i=0;i<Customer.no_of_books;i++){
-                if(Customer.books[i].equals(book));
-                flag=1;
-                list_of_books.add(book);
-                System.out.println("Thank you for returning the book.");
-                status_msg="Thank you for returning the book";
-
-            }
-            if(flag==0)
-                status_msg="That is not a valid book to return";
-
-        }
-        //System.out.print("Thank you for returning the book.");
-        return list_of_books;
-
-
+        else
+            return "That is not a valid book to return";
     }
+
+
 }
--- trunk/test/com/twu/biblioteca/LibraryTest.java
+++ trunk/test/com/twu/biblioteca/LibraryTest.java
@@ -1,62 +1,47 @@
 package com.twu.biblioteca;
 
-import com.twu.biblioteca.Book;
 import com.twu.biblioteca.Customer;
 import com.twu.biblioteca.Library;
 import org.junit.Test;
 
-import java.lang.reflect.Array;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 /**
- * Created by deeptibn on 2/25/14.
+ * Created by deeptibn on 2/27/14.
  */
-public class LibraryTest {
+public class LibraryTest{
 
     @Test
-    public void test_List_Of_Books(){
-        Library lib=new Library();
-        //System.out.println(lib.FetchList().toString());
-        assertEquals(lib.FetchList().toString(),"[Da Vinci Code, The Alchemist, Harry Potter]");
+    public void testIfListOfBooksisDisplayed(){
+        Library library = new Library(new Customer());
+        assertEquals(library.DisplayListOfBooks(),"[Alchemist, Inferno]");
     }
 
     @Test
-    public void test_Check_Out_Successful(){
-        Library lib=new Library();
-        //Customer cust=new Customer();
-      assertEquals(lib.CheckOut("Da Vinci Code").toString(), "[The Alchemist, Harry Potter]");
-        assertEquals(Customer.books[--Customer.no_of_books], "Da Vinci Code");
-        assertEquals(Library.status_msg,"Thank you! Enjoy the book");
-
+    public void testIfCheckOutIsSuccessful(){
+        Library library = new Library(new Customer());
+        assertEquals(library.CheckOut("Alchemist"),"Thank you! Enjoy the book");
     }
 
     @Test
-    public void test_Check_Out_UnSuccessful(){
-        Library lib=new Library();
-        //Customer cust=new Customer();
-        assertEquals(lib.CheckOut("Kite Runner").toString(), "[Da Vinci Code, The Alchemist, Harry Potter]");
-        assertEquals(Library.status_msg,"That book is not available");
-
+    public void testIfCheckOutIsUnSuccessful(){
+        Library library = new Library(new Customer());
+        assertEquals(library.CheckOut("Lajja"), "That book is not available");
     }
 
     @Test
-    public void test_Check_In(){
-        Library lib=new Library();
-        assertEquals(lib.CheckIn("Da Vinci Code").toString(), "[Da Vinci Code, The Alchemist, Harry Potter]");
-        for (int i=0;i<Customer.no_of_books;i++){
-            assertNotEquals(Customer.books[i],"Da Vinci Code");
-        }
-        assertEquals(Library.status_msg,"Thank you for returning the book");
+    public void testIfCheckInIsSuccessful(){
+        Library library = new Library(new Customer());
+        library.CheckOut("Alchemist");
+        assertEquals(library.CheckIn("Alchemist"), "Thank you for returning the book");
     }
 
     @Test
-    public void test_Check_In_UnSuccessful(){
-        Library lib=new Library();
-        //Customer cust=new Customer();
-        assertEquals(lib.CheckIn("Kite Runner").toString(), "[Da Vinci Code, The Alchemist, Harry Potter]");
-        assertEquals(Library.status_msg,"That is not a valid book to return");
-
+    public void testIfCheckInIsUnSuccessful(){
+        Library library = new Library(new Customer());
+        library.CheckOut("Alchemist");
+        assertEquals(library.CheckIn("Lajja"),"That is not a valid book to return");
     }
+
+
 }
